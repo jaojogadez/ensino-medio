aefe66e6942e69a22d948b586f831b0fba347a9f
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-10-13 01:35:17 -0300

    up

--- 2º Série - STI/Prof. Lucas/2º Semestre/Projeto - EchoLigths/js/script.js
+++ 2º Série - STI/Prof. Lucas/2º Semestre/Projeto - EchoLigths/js/script.js
@@ -9,3 +9,31 @@ window.addEventListener('scroll', function() {
         };
     });
     
+    document.addEventListener('DOMContentLoaded', function () {
+        const sections = document.querySelectorAll('.row');
+        const navLinks = document.querySelectorAll('.nav-link');
+      
+        const observerOptions = {
+          root: null, // viewport
+          threshold: 0.5, // Ativa quando 50% da seção estiver visível
+        };
+      
+        const observer = new IntersectionObserver(function (entries) {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              const sectionId = entry.target.id;
+              navLinks.forEach(link => {
+                link.classList.remove('active');
+                if (link.getAttribute('href') === `#${sectionId}`) {
+                  link.classList.add('active');
+                }
+              });
+            }
+          });
+        }, observerOptions);
+      
+        sections.forEach(section => {
+          observer.observe(section);
+        });
+      });
+      
