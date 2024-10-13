759956afb56a976d679f17dcc404d4cdfcf6db87
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-10-13 18:22:00 -0300

    update

--- 2º Série - STI/Prof. Lucas/2º Semestre/Projeto - EchoLigths/js/script.js
+++ 2º Série - STI/Prof. Lucas/2º Semestre/Projeto - EchoLigths/js/script.js
@@ -1,39 +1,42 @@
 window.addEventListener('scroll', function() {
-    var header = document.querySelector('.menu--movel');
-    var scrollTop = window.scrollY;
+    let header = document.querySelector('.menu--movel');
+    let scrollTop = window.scrollY;
     
         if (scrollTop > 70) {
             header.classList.add('menu--show');
         } else {
             header.classList.remove('menu--show');
         };
-    });
-    
-    document.addEventListener('DOMContentLoaded', function () {
-        const sections = document.querySelectorAll('.row');
-        const navLinks = document.querySelectorAll('.nav-link');
-      
-        const observerOptions = {
-          root: null, // viewport
-          threshold: 0.5, // Ativa quando 50% da seção estiver visível
-        };
-      
-        const observer = new IntersectionObserver(function (entries) {
-          entries.forEach(entry => {
-            if (entry.isIntersecting) {
-              const sectionId = entry.target.id;
-              navLinks.forEach(link => {
-                link.classList.remove('active');
-                if (link.getAttribute('href') === `#${sectionId}`) {
-                  link.classList.add('active');
-                }
-              });
-            }
+});
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const sections = document.querySelectorAll('.row');
+  const navLinks = document.querySelectorAll('.nav-link');
+        
+          const observerOptions = {
+            root: null, // viewport
+            threshold: 0.5, // Ativa quando 50% da seção estiver visível
+          };
+        
+          const observer = new IntersectionObserver(function (entries) {
+            entries.forEach(entry => {
+              if (entry.isIntersecting) {
+                const sectionId = entry.target.id;
+                navLinks.forEach(link => {
+                  link.classList.remove('active');
+                  if (link.getAttribute('href') === `#${sectionId}`) {
+                    link.classList.add('active');
+                  }
+                });
+              }
+            });
+          }, observerOptions);
+        
+          sections.forEach(section => {
+            observer.observe(section);
           });
-        }, observerOptions);
-      
-        sections.forEach(section => {
-          observer.observe(section);
-        });
-      });
+});
       
