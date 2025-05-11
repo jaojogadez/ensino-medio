c742650f1fdd3b2b09121af3d53f9c4df51ada67
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2025-05-11 20:43:49 -0300

    refatore: esrtutura de pastas

---
+++ 3º Série - IVS/python/exercices/ex_01.py
@@ -0,0 +1,4 @@
+''' 
+Crie um programa que simule a rolagem de dados utilizados em jogos de RPG, como D4, D6, D8, D10, D12, D20 e D100. Você pode permitir que o usuário escolha o tipo de dado e o número de vezes que deseja lançá-lo. Este projeto é excelente para praticar estruturas condicionais e loops
+
+'''
